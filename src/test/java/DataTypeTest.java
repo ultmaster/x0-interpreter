bc6829bc164e259bfbc9041ac2f856232e6f72e1
ultmaster <scottyugochang@hotmail.com>
2018-12-08 15:56:46 +0800

    add: data type test

--- src/test/java/DataTypeTest.java
+++ src/test/java/DataTypeTest.java
@@ -4,37 +4,48 @@ import core.data.*;
 import org.junit.Test;
 
 public class DataTypeTest {
-    private ElementaryType getType(int i) {
-        if (i == -1) return new X0String("Hello");
+    private ElementaryType getType(int i, boolean t) {
+        if (i == -1) return new X0String(t ? "1" : "Hello");
         if (i == 0) return new X0Boolean(1);
-        if (i == 1) return new X0Integer(2);
-        if (i == 2) return new X0Float(2.8);
+        if (i == 1) return new X0Integer(t ? 1 : 2);
+        if (i == 2) return new X0Float(t ? 1.0 : 2.8);
         return null;
     }
 
     @Test
     public void castDataType() {
-        for (int i = -1; i <= 2; ++i)
-            for (int j = -1; j <= 2; ++j) {
-                ElementaryType a = getType(i);
-                ElementaryType b = getType(j);
+        for (boolean t: new Boolean[]{false, true}) {
+            for (int i = -1; i <= 2; ++i)
+                for (int j = -1; j <= 2; ++j) {
+                    ElementaryType a = getType(i, t);
+                    ElementaryType b = getType(j, t);
 
-                ElementaryType add_result = a.add(b);
-                System.out.println(String.format("%s + %s = %s", a.toString(), b.toString(), add_result.toString()));
-                try {
-                    ElementaryType subtract_result = a.subtract(b);
-                    System.out.println(String.format("%s - %s = %s", a.toString(), b.toString(), subtract_result.toString()));
-                } catch (RuntimeException e) {
-                    System.out.println(String.format("%s - %s = %s", a.toString(), b.toString(), e.toString()));
-                }
-                if (a instanceof X0Number && b instanceof X0Number) {
-                    ElementaryType mul_result = ((X0Number) a).multiply((X0Number) b);
-                    System.out.println(String.format("%s * %s = %s", a.toString(), b.toString(), mul_result.toString()));
-                    if (!(a instanceof X0Boolean && b instanceof X0Boolean)) {
-                        ElementaryType divide_result = ((X0Number) a).divide((X0Number) b);
-                        System.out.println(String.format("%s / %s = %s", a.toString(), b.toString(), divide_result.toString()));
+                    if (!t) {
+                        ElementaryType add_result = a.add(b);
+                        System.out.println(String.format("%s + %s = %s", a.toString(), b.toString(), add_result.toString()));
+                        try {
+                            ElementaryType subtract_result = a.subtract(b);
+                            System.out.println(String.format("%s - %s = %s", a.toString(), b.toString(), subtract_result.toString()));
+                        } catch (RuntimeException e) {
+                            System.out.println(String.format("%s - %s = %s", a.toString(), b.toString(), e.toString()));
+                        }
+                        if (a instanceof X0Number && b instanceof X0Number) {
+                            ElementaryType mul_result = ((X0Number) a).multiply((X0Number) b);
+                            System.out.println(String.format("%s * %s = %s", a.toString(), b.toString(), mul_result.toString()));
+                            if (!(a instanceof X0Boolean && b instanceof X0Boolean)) {
+                                ElementaryType divide_result = ((X0Number) a).divide((X0Number) b);
+                                System.out.println(String.format("%s / %s = %s", a.toString(), b.toString(), divide_result.toString()));
+                            }
+                        }
+                    } else {
+                        try {
+                            int compareResult = a.compare(b);
+                            System.out.println(String.format("%s == %s = %d", a.getClass().toString(), b.getClass().toString(), compareResult));
+                        } catch (RuntimeException e) {
+                            System.out.println(String.format("%s == %s = failed", a.getClass().toString(), b.getClass().toString()));
+                        }
                     }
                 }
-            }
+        }
     }
 }
